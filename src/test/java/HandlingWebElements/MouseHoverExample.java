f0f210e628a7851b666ec61cc52ce6c023ed145e
Punam Basnet <hw819078@gmail.com>
2026-09-21 18:01:24 +0545

    New Changes

--- src/test/java/HandlingWebElements/MouseHoverExample.java
+++ src/test/java/HandlingWebElements/MouseHoverExample.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class MouseHooverExample {
+public class MouseHoverExample {
     public static void main(String[] args) {
 
         WebDriver driver = new ChromeDriver();
@@ -17,7 +17,7 @@ public class MouseHooverExample {
         WebElement mouseHover = driver.findElement(By.xpath("//h2[text()='Mouse Hover']"));
 
         JavascriptExecutor jse = (JavascriptExecutor) driver;
-        jse.executeScript("arguments[0].scrollIntoView()",MouseHoverText);
+        jse.executeScript("arguments[0].scrollIntoView()",mouseHover);
 
 
     }
